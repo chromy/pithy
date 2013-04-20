e3029f3b426e04cf9bf62fb96a8c1e3c81d96a3f
Hector Dearman <hector.dearman@gmail.com>
2013-04-20 22:16:34 +0100

    refactored test

--- tests/test_whole_system.py
+++ tests/test_whole_system.py
@@ -2,10 +2,19 @@ import pithy
 from attest import Tests
 system = Tests()
 
-@system.test
-def should_greet_us():
-    pithy.app.config['TESTING'] = True
+# Turn on testing.
+pithy.app.config['TESTING'] = True
+
+@system.context
+def connect():
     app = pithy.app.test_client()
+    try:
+        yield app
+    finally:
+        pass
+
+@system.test
+def should_greet_us(app):
     response = app.get('/')
     assert 'Hello World' in response.data
 
