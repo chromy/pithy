dbe4cb0e576046925c2296539703c7a6c0651802
Hector Dearman <hector.dearman@gmail.com>
2013-04-21 20:19:21 +0100

    implement basic shortening

--- pithy/__init__.py
+++ pithy/__init__.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, flash, redirect, \
+    abort, url_for
 
-app = Flask(__name__)
+app = Flask(__name__.split('.')[0])
+app.secret_key = 'a'
 
-@app.route('/')
+URLS = {}
+
+@app.route('/',  methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        short_url = shorten_link(request.form['url'])
+        flash_short_url(short_url)
     return render_template('index.html')
+
+@app.route('/<short_url>')
+def redirect_short_url(short_url):
+    if short_url in URLS:
+        return redirect(URLS[short_url], code=302)
+    else:
+        abort(404)
+
+def shorten_link(url):
+    short_url = str(hash(url))
+    URLS[short_url] = url
+    return short_url
+
+def flash_short_url(short_url):
+    link = url_for('redirect_short_url', short_url=short_url)
+    s = 'Short url: <a href="{link}" class="short-link">{link}</a>'
+    flash(s.format(link=link))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tests/test_whole_system.py
+++ tests/test_whole_system.py
@@ -1,9 +1,18 @@
 import pithy
+from flask import redirect
+
+from bs4 import BeautifulSoup
 from attest import Tests
 system = Tests()
 
 # Turn on testing.
 pithy.app.config['TESTING'] = True
+
+def extract_short_url(data):
+    soup = BeautifulSoup(data)
+    links = soup.findAll('a', {'class':'short-link'})
+    assert len(links) == 1
+    return links[0].get_text()
 
 @system.context
 def connect():
@@ -17,6 +26,19 @@ def connect():
 def should_greet_us(app):
     response = app.get('/')
     assert 'Hello World' in response.data
+
+@system.test
+def should_shorten_url(app):
+    response = app.post('/', data={'url':'http://www.example.com'})
+    short_url = extract_short_url(response.data)
+    response = app.get(short_url)
+    assert response.status_code == 302
+    assert response.location == 'http://www.example.com'
+
+@system.test
+def should_404_unused_short_urls(app):
+    response = app.get('/abcde')
+    assert response.status_code == 404
 
 if __name__ == '__main__':
     system.run()
